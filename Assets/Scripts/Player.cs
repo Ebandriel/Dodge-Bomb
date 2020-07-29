6ef5be92e38cfdf7be472318de8cc911b8c2cf62
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-07-29 21:38:25 +0100

    added player and movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private float minX = -2.8f;
+    private float maxX = 2.8f;
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 temp = transform.position;
+        if(h>0)
+        {
+            temp.x += speed * Time.deltaTime;
+            if (temp.x >= maxX)
+            {
+                temp.x = maxX;
+            }
+        }
+        else if(h<0)
+        {
+            temp.x -= speed * Time.deltaTime;
+            if(temp.x <= minX)
+            {
+                temp.x = minX;
+            }
+        }
+        transform.position = temp;
+
+    }
+}
