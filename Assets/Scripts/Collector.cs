1b8060fc50fe1ee42ba2caed550d00a63834922e
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-07-29 22:17:19 +0100

    completed bombdrop minigame

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+    void IncreaseScore()
+    {
+        score++;
+
+        scoreText.text = "score : " + score;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bomb")
+        {
+            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+            IncreaseScore();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject _bombPrefab;
+
+    private float minX = -2.8f;
+    private float maxX = 2.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnBombs());
+    }
+
+
+    IEnumerator SpawnBombs()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+        Instantiate(_bombPrefab, new Vector2(Random.Range(minX, maxX), transform.position.y), quaternion.identity);
+
+        StartCoroutine(SpawnBombs());
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
